915ffbda8b887d54209e8505970d1f563953a84e
ghiedoc <ghiedoc@gmail.com>
2020-10-26 15:11:32 +0800

    added quiz container

---
+++ Science Page/jsp_Science/question.js
@@ -0,0 +1,56 @@
+// Array of Questions
+
+const quiz = [{
+        q: 'What is the nose used for?',
+        options: ['Tasting', 'Touching', 'Smelling', 'Hearing'],
+        answer: 2
+    },
+    {
+        q: 'What is the tongue used for?',
+        options: ['Tasting', 'Touching', 'Seeing', 'Hearing'],
+        answer: 0
+    },
+    {
+        q: 'What is the hand used for?',
+        options: ['Tasting', 'Touching', 'Smelling', 'Hearing'],
+        answer: 1
+    },
+    {
+        q: 'What is the ear used for?',
+        options: ['Seeing', 'Touching', 'Smelling', 'Hearing'],
+        answer: 3
+    },
+    {
+        q: 'What is the eyes used for?',
+        options: ['Tasting', 'Touching', 'Smelling', 'Seeing'],
+        answer: 3
+    }
+]
+
+const quiz_living_nonliving = [{
+        q: 'Picture 1',
+        options: ['Living Things', 'Non-living Things'],
+        answer: 2
+    },
+    {
+        q: 'Picture 2',
+        options: ['Living Things', 'Non-living Things'],
+        answer: 2
+    },
+    {
+        q: 'Picture 3',
+        options: ['Living Things', 'Non-living Things'],
+        answer: 2
+    },
+    {
+        q: 'Picture 4',
+        options: ['Living Things', 'Non-living Things'],
+        answer: 2
+    },
+    {
+        q: 'Picture 5',
+        options: ['Living Things', 'Non-living Things'],
+        answer: 2
+    },
+
+]
