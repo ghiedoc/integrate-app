2e1eb4a01fb22f8f8e3df81488f714b75823f15e
ghiedoc <ghiedoc@gmail.com>
2020-11-09 12:41:56 +0800

    added js scripts

--- Science Page/jsp_Science/question.js
+++ Science Page/jsp_Science/question.js
@@ -1,56 +1,270 @@
 // Array of Questions
+const unit1 = [
+  {
+    q: "What is NOSE for?",
+    options: ["Tasting", "Hearing", "Smelling", "Seeing", "Touching"],
+    answer: 2,
+  },
+  {
+    q: "What is TONGUE for?",
+    options: ["Touching", "Hearing", "Seeing", "Smelling", "Tasting"],
+    answer: 4,
+  },
+  {
+    q: "What is HANDS for?",
+    options: ["Hearing", "Touching", "Smelling", "Seeing", "Tasting"],
+    answer: 4,
+  },
+  {
+    q: "What is EAR for?",
+    options: ["Seeing", "Touching", "Smelling", "Hearing", "Tasting"],
+    answer: 4,
+  },
+  {
+    q: "What is EYES for?",
+    options: ["Seeing", "Smelling", "Tasting", "Hearing", "Touching"],
+    answer: 4,
+  },
+  {
+    q: "Your ears help you hear sounds?",
+    options: ["True", "False"],
+    answer: 0,
+  },
+  {
+    q: "Your nose help you taste food?",
+    options: ["True", "False"],
+    answer: 1,
+  },
+  {
+    q: "Your tongue helps you taste different flavors of food?",
+    options: ["True", "False"],
+    answer: 0,
+  },
+  {
+    q: "Your skin helps you feel things?",
+    options: ["True", "False"],
+    answer: 0,
+  },
+];
 
-const quiz = [{
-        q: 'What is the nose used for?',
-        options: ['Tasting', 'Touching', 'Smelling', 'Hearing'],
-        answer: 2
-    },
-    {
-        q: 'What is the tongue used for?',
-        options: ['Tasting', 'Touching', 'Seeing', 'Hearing'],
-        answer: 0
-    },
-    {
-        q: 'What is the hand used for?',
-        options: ['Tasting', 'Touching', 'Smelling', 'Hearing'],
-        answer: 1
-    },
-    {
-        q: 'What is the ear used for?',
-        options: ['Seeing', 'Touching', 'Smelling', 'Hearing'],
-        answer: 3
-    },
-    {
-        q: 'What is the eyes used for?',
-        options: ['Tasting', 'Touching', 'Smelling', 'Seeing'],
-        answer: 3
-    }
-]
+const unit2 = [
+  {
+    q: "Which of the following are living thing?",
+    options: ["Elephant", "Pencil", "Chair", "Book"],
+    answer: 0,
+  },
+  {
+    q: "Which of the following are living thing?",
+    options: ["Bag", "Pencil", "Tree", "Book"],
+    answer: 2,
+  },
+  {
+    q: "Which of the following are living thing?",
+    options: ["Book", "Pencil", "Chair", "Plants"],
+    answer: 3,
+  },
+  {
+    q: "Which of the following are living thing?",
+    options: ["Book", "Pencil", "Human", "Ball"],
+    answer: 2,
+  },
+  {
+    q: "Which of the following are living thing?",
+    options: ["Book", "Bird", "Chair", "Hat"],
+    answer: 1,
+  },
+  {
+    q: "Non-Living things cannot grow?",
+    options: ["True", "False"],
+    answer: 0,
+  },
+  {
+    q: "Non-Living things can move?",
+    options: ["True", "False"],
+    answer: 1,
+  },
+  {
+    q: "Non-Living things cannot multiply on their own?",
+    options: ["True", "False"],
+    answer: 0,
+  },
+  {
+    q: "Is teddy bear a non-living thing?",
+    options: ["True", "False"],
+    answer: 0,
+  },
+  {
+    q: "Is plant a non-living thing?",
+    options: ["True", "False"],
+    answer: 1,
+  },
+];
 
-const quiz_living_nonliving = [{
-        q: 'Picture 1',
-        options: ['Living Things', 'Non-living Things'],
-        answer: 2
-    },
-    {
-        q: 'Picture 2',
-        options: ['Living Things', 'Non-living Things'],
-        answer: 2
-    },
-    {
-        q: 'Picture 3',
-        options: ['Living Things', 'Non-living Things'],
-        answer: 2
-    },
-    {
-        q: 'Picture 4',
-        options: ['Living Things', 'Non-living Things'],
-        answer: 2
-    },
-    {
-        q: 'Picture 5',
-        options: ['Living Things', 'Non-living Things'],
-        answer: 2
-    },
+const unit3 = [
+  {
+    q: "Holds the seeds.",
+    options: ["Fruit", "Root", "Leaf", "Flower", "Stem"],
+    answer: 0,
+  },
+  {
+    q: "Holds the plants in place.",
+    options: ["Fruit", "Leaf", "Root", "Flower", "Stem"],
+    answer: 2,
+  },
+  {
+    q: "Makes the food of the plant.",
+    options: ["Stem", "Flower", "Root", "Leaf", "Fruit"],
+    answer: 3,
+  },
+  {
+    q: "Produces the seeds and become fruits.",
+    options: ["Fruit", "Root", "Leaf", "Stem", "Flower"],
+    answer: 4,
+  },
+  {
+    q: "Brings the water and nutrients from the roots to the leaves.",
+    options: ["Leaf", "Fruit", "Root", "Flower", "Stem"],
+    answer: 4,
+  },
+  {
+    q: "The products produced by the flowering part of a plant.",
+    options: ["Leaf", "Fruit", "Flower", "Root", "Stem"],
+    answer: 1,
+  },
+  {
+    q: "Takes in sunlight.",
+    options: ["Leaf", "Fruit", "Root", "Stem", "Flower"],
+    answer: 0,
+  },
+  {
+    q: "Take the water from the soil.",
+    options: ["Fruit", "Leaf", "Root", "Stem", "Flower"],
+    answer: 2,
+  },
+  {
+    q: "Breathes in air.",
+    options: ["Flower", "Stem", "Root", "Fruits", "Leaf"],
+    answer: 4,
+  },
+  {
+    q: "Produces the seeds.",
+    options: ["Stem", "Flower", "Leaf", "Fruits", "Rootf"],
+    answer: 3,
+  },
+];
 
-]
+const unit4 = [
+  {
+    q: "Animals that cannot become pets and cannot live inside the house.",
+    options: ["TAME ANIMALS", "WILD ANIMALS"],
+    answer: 1,
+  },
+  {
+    q: "Animals that can become pets and can be useful to man.",
+    options: ["TAME ANIMALS", "WILD ANIMALS"],
+    answer: 0,
+  },
+  {
+    q: "DOG",
+    options: ["TAME ANIMAL", "WILD ANIMAL"],
+    answer: 0,
+  },
+  {
+    q: "LION",
+    options: ["TAME ANIMAL", "WILD ANIMAL"],
+    answer: 1,
+  },
+  {
+    q: "TIGER",
+    options: ["TAME ANIMAL", "WILD ANIMAL"],
+    answer: 1,
+  },
+  {
+    q: "What do RABBIT eats",
+    options: ["carrot", "fish", "rice", "bone"],
+    answer: 0,
+  },
+  {
+    q: "What do CAT eats",
+    options: ["leaf", "fish", "grass", "bone"],
+    answer: 1,
+  },
+  {
+    q: "BEAR",
+    options: ["piglet", "cub", "calf", "kid"],
+    answer: 1,
+  },
+  {
+    q: "CAT",
+    options: ["kitten", "cub", "calf", "kid"],
+    answer: 0,
+  },
+  {
+    q: "PIG",
+    options: ["calf", "cub", "piglet", "kid"],
+    answer: 0,
+  },
+];
+
+const unit5 = [
+  {
+    q:
+      "An air transportation is a transportation for moving a passenger by air vehicle.",
+    options: ["True", "False"],
+    answer: 0,
+  },
+  {
+    q:
+      "A maritime trasportation is a transportation for moving passengers by water vehicle.",
+    options: ["True", "False"],
+    answer: 0,
+  },
+  {
+    q:
+      "A road transportation is a vehicles which run on tracks (rails or railroad).",
+    options: ["True", "False"],
+    answer: 1,
+  },
+  {
+    q:
+      "Mountains that sometimes release steam and lava from the inside of the Earth.",
+    options: ["Valley", "Hills", "Volcano", "Plain"],
+    answer: 2,
+  },
+  {
+    q: "Large areas of land that are mostly flat.",
+    options: ["Volcano", "Plain", "Mountain", "Hills"],
+    answer: 1,
+  },
+  {
+    q: "Is a landform that rises high above its surroundings.",
+    options: ["Mountain", "Valley", "Volcano", "Hills"],
+    answer: 0,
+  },
+  {
+    q: "Is a landform that rises high above its surroundings.",
+    options: ["Mountain", "Valley", "Volcano", "Hills"],
+    answer: 0,
+  },
+  {
+    q: "A water flowing from the surface of the Earth.",
+    options: ["Ocean", "Sea", "River", "Lake"],
+    answer: 2,
+  },
+  {
+    q: "The largest type of water bodies.",
+    options: ["Ocean", "Sea", "River", "Lake"],
+    answer: 0,
+  },
+  {
+    q: "An area where water flows over a vertical drop.",
+    options: ["Waterfalls", "Sea", "Lake", "River"],
+    answer: 0,
+  },
+  {
+    q:
+      "Represents a body of either fresh or salt water completely enclosed by land.",
+    options: ["Lake", "Sea", "River", "Ocean"],
+    answer: 0,
+  },
+];
